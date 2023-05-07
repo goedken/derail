e8105a37e6f6a64134a1c2c7a2a5bc244f2a1c08
goedken <alexgoedken@gmail.com>
2023-05-06 19:00:08 -0500

    remove passport, derail first track

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # derail
+
+## Get started
+
+Run `yarn` to install dependencies
+
+`npm run build` + `npm run dev` to run the app and watch for changes
+
+Navigate to `http://localhost:3000`
--- index.ts
+++ index.ts
@@ -1,121 +1,69 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import passport from "passport";
-import { Profile, Strategy as SpotifyStrategy, VerifyCallback } from "passport-spotify";
-import session from "express-session";
-import SpotifyWebApi from "spotify-web-api-node";
+import axios, { AxiosResponse } from "axios";
+import qs from "qs";
+
+const SPOTIFY_BASE_URL = "https://api.spotify.com/v1";
 
 dotenv.config();
 const app: Express = express();
 
 const port = process.env.PORT;
 
-const clientId = process.env.SPOTIFY_CLIENT_ID;
-const clientSecret = process.env.SPOTIFY_SECRET;
-const authCallbackPath = "/auth/spotify/callback";
-
-if (!clientId || !clientSecret) {
-  console.log("No client id or client secret. Exiting...");
-  process.exit();
-}
-
-passport.serializeUser((user: object, done: VerifyCallback) => {
-  done(null, user);
+app.get("/", async (_req: Request, res: Response) => {
+  res.send("Derail");
 });
 
-passport.deserializeUser((obj: object, done: VerifyCallback) => {
-  done(null, obj);
-});
-
-const spotifyApi = new SpotifyWebApi({
-  clientId,
-  clientSecret,
-  redirectUri: 'http://localhost:3000/'
-});
-
-passport.use(
-  new SpotifyStrategy(
-    {
-      clientID: clientId,
-      clientSecret: clientSecret,
-      callbackURL: `http://localhost:${port}${authCallbackPath}`,
-      passReqToCallback: true
-    },
-    async (
-      req: Request,
-      accessToken: string,
-      refreshToken: string,
-      expires_in: number,
-      profile: Profile,
-      done
-    ) => {
-      console.log(JSON.stringify(profile))
-      process.nextTick(() => {
-        spotifyApi.setAccessToken(accessToken)
-        return done(null, profile);
-      });
-    }
-  )
-);
-
-app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
-
-app.get("/oops", (req: Request, res: Response) => {
-  res.send("FUCK");
-});
-
-app.get(
-  "/auth/spotify",
-  passport.authenticate("spotify", {
-    scope: ["user-read-email", "user-read-private"],
-  })
-);
-
-app.get(
-  authCallbackPath,
-  passport.authenticate("spotify", { failureRedirect: "/oops", successMessage: true, failureMessage: true }),
-  async (req: Request, res: Response) => {
-    res.redirect("/yay");
+app.get("/derail", async (req: Request, res: Response) => {
+  const { playlistId } = req.query;
+  const token = await getAuthorizationToken();
+  const playlistResponse: AxiosResponse<SpotifyApi.PlaylistTrackResponse> = await axios.get(
+    `${SPOTIFY_BASE_URL}/playlists/${playlistId}/tracks`,
+    { headers: { Authorization: `Bearer ${token}` } }
+  );
+  const tracks = playlistResponse.data;
+  const firstTrack = tracks.items[0].track;
+  if (!firstTrack) {
+    return res.send('fuck :(')
   }
-);
-
-app.get('/yay', (req: Request, res: Response) => {
-  res.send(":)");
-});
-
-app.get(
-  "/me",
-  ensureAuthenticated,
-  async (req: Request, res: Response) => {
-    console.log('we tryin this shit or what')
-    const radioheadAlbums = await spotifyApi.getArtistAlbums(
-      "4Z8W4fKeB5YxbusRsdQVPb"
-    );
-    res.send(JSON.stringify(radioheadAlbums));
-    // const me = await spotifyApi.getMe();
-    // console.log("ME", me)
-    // res.send(JSON.stringify(me))
-    //https://open.spotify.com/artist/4Z8W4fKeB5YxbusRsdQVPb?si=RzMU86GlTw-Su3-EAkamEA
+  const nextTrack = await getNextTrackOffAlbum(firstTrack, token);
+  if (!nextTrack) {
+    return res.send('fuckkkkkkkkk :((((((((')
   }
-);
+  res.send(`${firstTrack.name} was derailed to ${nextTrack.name}`);
+});
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
 
-async function ensureAuthenticated(req: Request, res: Response, done: VerifyCallback) {
-  if (req.isAuthenticated()) {
-    console.log('is authenticated')
-    return done();
+async function getAuthorizationToken() {
+  const response = await axios.post(
+    "https://accounts.spotify.com/api/token",
+    qs.stringify({
+      grant_type: "client_credentials",
+      client_id: process.env.SPOTIFY_CLIENT_ID,
+      client_secret: process.env.SPOTIFY_SECRET,
+    }),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  return response.data.access_token;
+}
+
+async function getNextTrackOffAlbum(track: SpotifyApi.TrackObjectFull, token: string): Promise<SpotifyApi.TrackObjectSimplified | undefined> {
+  const albumHref = track.album.href;
+  const trackNumber = track.track_number;
+  const albumResponse: AxiosResponse<SpotifyApi.AlbumTracksResponse> = await axios.get(`${albumHref}/tracks`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  const albumTracks = albumResponse.data.items;
+  if (trackNumber > albumTracks.length) {
+    return albumTracks[0];
   }
-  res.redirect("/oops");
+
+  return albumTracks.find((track: any) => track.track_number === trackNumber + 1)
 }
